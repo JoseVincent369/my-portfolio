47a87103bd1d85663fe6f10bf96e0ffdb78b0b0c
JoseVincent369 <20211472>
2025-10-03 02:05:08 +0800

    t

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,37 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    VitePWA({
+      manifest: {
+        includeAssets: ["favicon.ico", "apple-touch-icon.png", "assets/*"],
+        name: "Solution System Portal",
+        short_name: "SSP",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "/favicon.ico",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/dazzle.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ["*/.{js,css,html,ico,png,svg,webmanifest}"],
+      },
+    }),
+
+    { enforce: "pre" },
+    react({ include: /\.(js|jsx|ts|tsx)$/ }),
+  ],
+});
